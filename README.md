c8753c201c3862a99da60c6601c79433517ece56
mawilson1234 <michael.a.wilson@yale.edu>
2024-11-07 21:09:06 -0500

    add preprocessing script

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a PCIbex implementation of a self-paced reading with comprehension questions task. The experimental stimuli to be presented should be entered into `chunk_includes/stimuli.csv`. The experiment records reading times per word, question responses, response times, and condition labels for all stimuli.
 
+`preprocess.r` is an R script that will preprocess the results from PCIbex into an easier-to-use format.
+
 This experiment is currently used as part of CGSC/LING 496/696 (Wilson) at the University of Delaware. It is distributed under the GNU General Public License v3.0.
---
+++ preprocess.r
@@ -0,0 +1,100 @@
+library(tidyverse)
+library(data.table)
+
+# User-defined function to read in PCIbex Farm results files
+read.pcibex <- function(
+	filepath, 
+	colnames.from,
+	auto.colnames = TRUE, 
+	fun.col = \(col, cols) {
+			cols[cols == col] <- paste(col, 'Ibex', sep = '.')
+			return (cols)
+		}
+	) {
+	
+	get.colnames <- function(filepath) {
+		cols <- c()
+		con <- file(filepath, 'r')
+		while (TRUE) {
+			line <- readLines(con, n = 1, warn = FALSE)
+			if (length(line) == 0) {
+				break
+			}
+			
+			m <- regmatches(line, regexec(r'(^# (\d+)\. (.+)\.,*?$)', line))[[1]]
+			if (length(m) == 3) {
+				index <- as.numeric(m[2])
+				value <- m[3]
+				if (is.function(fun.col)) {
+					cols <- fun.col(value, cols)
+				}
+				cols[index] <- value
+				if (index == n.cols) {
+					break
+				}
+			}
+		}
+		close(con)
+		
+		return (cols)
+	}
+	
+	n.cols <- max(count.fields(filepath, sep = ',', quote = NULL), na.rm = TRUE)
+	if (auto.colnames) {
+		cols <- get.colnames(filepath)
+		
+		# this happens due to a data coding errors we've fixed
+		if (is.null(cols)) {
+			cols <- get.colnames(colnames.from)
+		}
+		
+		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = cols))
+	}
+	else {
+		return (read.csv(filepath, comment.char = '#', header = FALSE, col.names = seq_len(n.cols)))
+	}
+}
+
+df <- read.pcibex('results.csv') |>
+	as_tibble() |>
+	mutate(
+		participant = paste0(Results.reception.time, MD5.hash.of.participant.s.IP.address),
+		participant = match(participant, unique(participant))
+	) |>
+	select(-Results.reception.time, -MD5.hash.of.participant.s.IP.address) |>
+	select(participant, everything())
+
+# pull out sentences and questions separately, then rejoin
+reading <- df |>
+	filter(PennElementName == 'EPDashedSentence') |>
+	select(
+		participant, Parameter, Value, 
+		item, sentence, condition, Reading.time
+	) |>
+	rename(
+		word_number = Parameter,
+		word = Value,
+		reading_time = Reading.time
+	)
+
+questions <- df |>
+	filter(PennElementName == 'QuestionAlt') |>
+	select(
+		participant, Value, item:Newline.
+	) |>
+	rename(
+		response = Value,
+		accuracy = Reading.time,
+		response_time = Newline.
+	) |>
+	select(
+		participant, item:question,
+		correct_answer, response, accuracy:response_time
+	)
+
+# recombine
+df <- reading |>
+	left_join(questions)
+
+df |>
+	fwrite('cleaned_results.csv', row.names = FALSE)
